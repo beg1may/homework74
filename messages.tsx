63e5ecd0627eb496f9154ef180a2cf0245804c95
Silalieva Begimay <begimayomurovna@gmail.com>
2024-03-19 18:39:01 +0600

    При получении новых данных записываются в файл

--- messages.tsx
+++ messages.tsx
@@ -1,3 +1,10 @@
-{
-  "message": "Hello, world!"
-}
+[
+  {
+    "message": "Hello, !",
+    "date": "2024-03-19T12:34:46.860Z"
+  },
+  {
+    "message": "Hello",
+    "date": "2024-03-19T12:34:46.860Z"
+  }
+]
--- messages/message.ts
+++ messages/message.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import {Message} from '../types';
+import {promises as fs} from 'fs';
+
+const path = './messages.tsx';
 
 const messageRouter = express.Router();
 
@@ -19,7 +22,19 @@ messageRouter.post('/:id', (req, res) => {
     message: req.body.message,
     date: datetime,
   });
+
+  const run = async () => {
+    try {
+      await fs.writeFile(path, JSON.stringify(messages, null, 2))
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  run().catch(console.error);
+
   return res.send('Ты отправил POST запрос');
 });
 
 export default messageRouter;
+
